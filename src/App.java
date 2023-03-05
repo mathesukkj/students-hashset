41398bb929475d872431cd76e962e51e9d47caa1
mathesukkj <kemuel.g7363@gmail.com>
2023-03-05 13:28:48 -0300

    feat: add app basic functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner s = new Scanner(System.in)) {
+
+            Set<User> set = new HashSet<>();
+
+            System.out.print("How many students for Course A? ");
+            int n = s.nextInt();
+            for (int i = 0; i < n; i++) {
+                set.add(new User(s.nextInt()));
+            }
+
+            System.out.print("How many students for Course B? ");
+            n = s.nextInt();
+            for (int i = 0; i < n; i++) {
+                set.add(new User(s.nextInt()));
+            }
+
+            System.out.print("How many students for Course C? ");
+            n = s.nextInt();
+            for (int i = 0; i < n; i++) {
+                set.add(new User(s.nextInt()));
+            }
+
+            System.out.print("Total students: ");
+            System.out.println(set.size());
+        }
     }
 }
---
+++ src/User.java
@@ -0,0 +1,38 @@
+public class User {
+    private int code;
+
+    public User(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + code;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (code != other.code)
+            return false;
+        return true;
+    }
+
+}
